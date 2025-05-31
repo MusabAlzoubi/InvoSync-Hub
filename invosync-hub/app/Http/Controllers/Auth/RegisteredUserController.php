b0ef33ee26cc4dc83f32842a41aeacc3007e875b
islamatiyat01 <islamatiyat50@yahoo.com>
2025-05-31 21:46:14 +0300

    request company id in register

--- invosync-hub/app/Http/Controllers/Auth/RegisteredUserController.php
+++ invosync-hub/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -39,6 +39,8 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'company_id' => $request->company_id,
+
         ]);
 
         event(new Registered($user));
